edb9f163e29874218631fc06c3b97a647f6ebadb
Rahul Ravishankar <rahulrshankar02@gmail.com>
2020-12-20 21:46:25 +0530

    Initial commit

---
+++ src/LeapYear.java
@@ -0,0 +1,17 @@
+public class LeapYear {
+
+    public static boolean isLeapYear (int year) {
+        if (year < 1 || year > 9999) {
+            System.out.println("Invalid Year");
+            return false;
+        }
+
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else if (year % 100 == 0 && year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
